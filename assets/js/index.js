235ad1a478e2bcef0ec79790a29dad695e96d3bd
Gabriel Dantas <dantas2009@gmail.com>
2023-05-27 13:25:22 -0300

    pagina index

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,8 +12,10 @@ async function buscar() {
 
         carregarCards(json.NUM_PRODUTOS, json.NUM_EMPRESAS, json.NUM_ARMAZENS);
         carregarAlertas(json.ALERTAS);
-        //carregarTransacoes(json.TRANSACOES);
+        carregarTransacoes(json.TRANSACOES);
     }
+
+    console.log("tes");
 }
 
 function carregarCards(produtos, armazens, empresas) {
@@ -29,6 +31,12 @@ function carregarCards(produtos, armazens, empresas) {
 }
 
 function carregarAlertas(alertas) {
+    if (alertas.length == 0) {
+        const ultimosAlertas = document.getElementById("ultimosAlertas");
+        ultimosAlertas.style.display = "none";
+        return;
+    }
+
     const tbody = document.getElementById("alertas");
 
     alertas.forEach(alerta => {
@@ -46,11 +54,13 @@ function carregarAlertas(alertas) {
 
         //TD QUANTIDADE ATUAL
         const tdAtual = document.createElement("td");
+        tdAtual.classList.add("text-center");
         tdAtual.innerText = alerta.NUM_QUANTIDADE_ATUAL;
         tr.appendChild(tdAtual);
 
         //TD QUANTIDADE MINIMA
         const tdMinima = document.createElement("td");
+        tdMinima.classList.add("text-center");
         tdMinima.innerText = alerta.NUM_QUANTIDADE_MINNIMA;
         tr.appendChild(tdMinima);
 
@@ -70,122 +80,91 @@ function carregarAlertas(alertas) {
     });
 }
 
-function carregar(empresas) {
-    const item = document.getElementById("item");
+function carregarTransacoes(transacoes) {
+    const tbody = document.getElementById("transacoes");
 
-    empresas.forEach(empresa => {
+    transacoes.forEach(transacao => {
         const tr = document.createElement("tr");
 
-        //TD ID
-        const tdId = document.createElement("td");
-        tdId.innerText = empresa.ID_EMPRESA;
-        tr.appendChild(tdId);
+        //TD TIPO
+        const tdTipo = document.createElement("td");
+        tdTipo.innerText = transacao.TXT_TIPO;
+        tr.appendChild(tdTipo);
 
-        //TD NOME
-        const tdNome = document.createElement("td");
-        tr.appendChild(tdNome);
-        tdNome.innerText = empresa.TXT_NOME;
+        //TD PRODUTO
+        const tdProduto = document.createElement("td");
+        tdProduto.innerText = transacao.TXT_PRODUTO;
+        tr.appendChild(tdProduto);
 
-        //TD VITRINE
-        const tdVitrine = document.createElement("td");
-        tdVitrine.classList.add("text-center");
-        const aVitrine = document.createElement("a");
-        aVitrine.href = `vitrine.html?id=${empresa.ID_EMPRESA}`;
-        aVitrine.classList.add("link");
-        const iVitrine = document.createElement("i");
-        iVitrine.classList.add("fa-solid", "fa-bars");
-        aVitrine.appendChild(iVitrine);
-        tdVitrine.appendChild(aVitrine);
-        tr.appendChild(tdVitrine);
+        //TD QUANTIDADE
+        const tdQuantidade = document.createElement("td");
+        tdQuantidade.classList.add("text-center");
+        tdQuantidade.innerText = transacao.NUM_QUANTIDADE;
+        tr.appendChild(tdQuantidade);
 
-        //TD EDITAR
-        const tdEditar = document.createElement("td");
-        tdEditar.classList.add("text-center");
-        const aEditar = document.createElement("a");
-        aEditar.href = `empresa.html?role=editar&id=${empresa.ID_EMPRESA}`;
-        aEditar.classList.add("link");
-        const iEditar = document.createElement("i");
-        iEditar.classList.add("fa-solid", "fa-pen-to-square");
-        aEditar.appendChild(iEditar);
-        tdEditar.appendChild(aEditar);
-        tr.appendChild(tdEditar);
+        //TD EMPRESA
+        const tdEmpresa = document.createElement("td");
+        tdEmpresa.innerText = transacao.TXT_EMPRESA;
+        tr.appendChild(tdEmpresa);
 
-        //TD EXCLUIR
-        const tdExcluir = document.createElement("td");
-        tdExcluir.classList.add("text-center");
-        const aExcluir = document.createElement("a");
-        aExcluir.href = "#";
-        aExcluir.classList.add("link");
-        const iExcluir = document.createElement("i");
-        iExcluir.classList.add("fa-solid", "fa-trash");
-        aExcluir.appendChild(iExcluir);
-        tdExcluir.appendChild(aExcluir);
+        //TD ARMAZEM
+        const tdArmazem = document.createElement("td");
+        tdArmazem.innerText = transacao.TXT_ARMAZEM;
+        tr.appendChild(tdArmazem);
 
-        tr.appendChild(tdExcluir);
+        //TD DESCRICAO 
+        const descricao = `${transacao.TXT_DESCRICAO.slice(0, 17)}...`;
+        const tdDescricao = document.createElement("td");
+        const aDescricao = document.createElement("a");
+        aDescricao.href = "#";
+        aDescricao.classList.add("link");
+        aDescricao.innerText = descricao;
+        tdDescricao.appendChild(aDescricao);
+        tr.appendChild(tdDescricao);
 
-        //MODAL REMOVER
-        if (empresa.BIT_DELETAR) {
-            aExcluir.dataset.bsToggle = "modal";
-            aExcluir.dataset.bsTarget = `#remover${empresa.ID_EMPRESA}`;
+        //MODAL DESCRICAO
+        aDescricao.dataset.bsToggle = "modal";
+        aDescricao.dataset.bsTarget = `#descricao${transacao.ID_PRODUTO}`;
 
-            const divModal = document.createElement("div");
-            divModal.classList.add("modal", "fade");
-            divModal.id = `remover${empresa.ID_EMPRESA}`;
-            divModal.tabIndex = "-1";
-            const divModalDialog = document.createElement("div");
-            divModalDialog.classList.add("modal-dialog");
-            const divModalContent = document.createElement("div");
-            divModalContent.classList.add("modal-content");
+        const divModal = document.createElement("div");
+        divModal.classList.add("modal", "fade");
+        divModal.id = `descricao${transacao.ID_PRODUTO}`;
+        divModal.tabIndex = "-1";
+        const divModalDialog = document.createElement("div");
+        divModalDialog.classList.add("modal-dialog");
+        const divModalContent = document.createElement("div");
+        divModalContent.classList.add("modal-content");
 
-            const divModalHeader = document.createElement("div");
-            divModalHeader.classList.add("modal-header");
-            const h5Title = document.createElement("h5");
-            h5Title.classList.add("modal-title");
-            h5Title.innerText = "Você tem certeza que deseja remover?";
-            const buttonClose = document.createElement("button");
-            buttonClose.classList.add("btn-close");
-            buttonClose.dataset.bsDismiss = "modal";
-            divModalHeader.appendChild(h5Title);
-            divModalHeader.appendChild(buttonClose);
+        const h5Title = document.createElement("h5");
+        h5Title.classList.add("modal-title");
+        h5Title.innerText = `Descrição ${transacao.TXT_PRODUTO}`;
+        const divModalHeader = document.createElement("div");
+        divModalHeader.classList.add("modal-header");
+        const buttonClose = document.createElement("button");
+        buttonClose.classList.add("btn-close");
+        buttonClose.dataset.bsDismiss = "modal";
+        divModalHeader.appendChild(h5Title);
+        divModalHeader.appendChild(buttonClose);
 
-            const divModalBody = document.createElement("div");
-            divModalBody.classList.add("modal-body");
-            const pbody = document.createElement("p");
-            pbody.innerHTML = `Nessa ação você irá remover <b>${empresa.TXT_NOME}</b>. Uma vez removido não poderá voltar atrás!`;
-            divModalBody.appendChild(pbody);
+        const divModalBody = document.createElement("div");
+        divModalBody.classList.add("modal-body");
+        const pbody = document.createElement("p");
+        pbody.innerHTML = transacao.TXT_DESCRICAO;
+        divModalBody.appendChild(pbody);
 
-            const divModalFooter = document.createElement("div");
-            divModalFooter.classList.add("modal-footer");
+        divModalContent.appendChild(divModalHeader);
+        divModalContent.appendChild(divModalBody);
 
-            const buttonFooterClose = document.createElement("button");
-            buttonFooterClose.classList.add("btn", "btn-secondary");
-            buttonFooterClose.dataset.bsDismiss = "modal";
-            buttonFooterClose.innerText = "Fechar";
-            divModalFooter.appendChild(buttonFooterClose);
+        divModalDialog.appendChild(divModalContent);
+        divModal.appendChild(divModalDialog);
+        tbody.appendChild(divModal);
 
-            const aFooterRemover = document.createElement("a");
-            aFooterRemover.href = "#";
-            aFooterRemover.addEventListener("click", function () { remover(empresa.ID_EMPRESA) });
+        //TD DATA
+        const tdData = document.createElement("td");
+        tdData.innerText = transacao.DAT_TRANSACAO;
+        tr.appendChild(tdData);
 
-            aFooterRemover.classList.add("btn", "btn-danger");
-            aFooterRemover.innerText = "Remover";
-            divModalFooter.appendChild(aFooterRemover);
-
-            divModalContent.appendChild(divModalHeader);
-            divModalContent.appendChild(divModalBody);
-            divModalContent.appendChild(divModalFooter);
-
-            divModalDialog.appendChild(divModalContent);
-            divModal.appendChild(divModalDialog);
-            item.appendChild(divModal);
-        } else {
-            aExcluir.dataset.bsToggle = "tooltip";
-            aExcluir.dataset.bsPlacement = "left";
-            aExcluir.title = "Empresa com produto relacionado, por favor exclua o produto primeiro.";
-            aExcluir.classList.add("disabled");
-        }
-
-        item.appendChild(tr);
+        tbody.appendChild(tr);
     });
 }
 
